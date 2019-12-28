f3b4d51923e15154df88eed6b7eb84af535d9c36
Shikhar Singh <shikharlx1010trex@gmail.com>
2019-12-28 18:09:27 +0530

    Removing Unnessery Lines

--- src/app.js
+++ src/app.js
@@ -6,12 +6,10 @@ const forecast = require('./utils/forecast')
 
 const app = express()
 
-// Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views')
 const partialsPath = path.join(__dirname, '../templates/partials')
 
-// Setup handlebars engine and views location
 app.set('view engine', 'hbs')
 app.set('views', viewsPath)
 hbs.registerPartials(partialsPath)
